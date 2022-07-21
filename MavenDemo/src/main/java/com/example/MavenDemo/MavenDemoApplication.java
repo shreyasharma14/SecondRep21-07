a411f360a8b22908e298ad89f9c555ffb303ae0c
shreyasharma14 <61916650+shreyasharma14@users.noreply.github.com>
2022-07-21 11:03:19 +0530

    Add files via upload

---
+++ MavenDemo/src/main/java/com/example/MavenDemo/MavenDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.MavenDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
